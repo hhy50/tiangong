93cbb30b6d2060df14399c40f83a89bff97c8982
haiyang <491680922@qq.com>
2024-04-17 20:56:19 +0800

    修改协议

--- transport/protocol/packet.go
+++ transport/protocol/packet.go
@@ -7,7 +7,6 @@ import (
 	"github.com/haiyanghan/tiangong/common"
 	"github.com/haiyanghan/tiangong/common/buf"
 	"github.com/haiyanghan/tiangong/common/errors"
-	"github.com/haiyanghan/tiangong/common/net"
 )
 
 const (
@@ -15,27 +14,24 @@ const (
 )
 
 type PacketHeader struct { // 20
-	Len      uint16        // 2
-	Rid      uint32        // 4
-	Protocol byte          // 1
-	Host     net.IpAddress // 4
-	Port     net.Port      // 2
-	Reserved [6]byte       // 6
-	Status   byte          // 1
+	Len      uint16   // 2
+	Rid      uint32   // 4
+	Protocol byte     // 1
+	Reserved [12]byte // 12
+	Status   byte     // 1
 }
 
-// func (h *PacketHeader) WriteTo(buffer buf.Buffer) error {
-// 	if buffer.Cap() < PacketHeaderLen {
-// 		return errors.NewError("write bytes len too short, minnum is "+strconv.Itoa(PacketHeaderLen)+"bytes", nil)
-// 	}
-// 	buf.WriteBytes(buffer, common.Uint16ToBytes(h.Len))
-// 	buf.WriteBytes(buffer, common.Uint32ToBytes(h.Rid))
-// 	buf.WriteByte(buffer, h.Protocol)
-// 	buf.WriteByte(buffer, h.HostLen)
-// 	buf.WriteBytes(buffer, common.Uint16ToBytes(h.Port))
-// 	buf.WriteBytes(buffer, h.Reserved[:])
-// 	return nil
-// }
+func (h *PacketHeader) WriteTo(buffer buf.Buffer) error {
+	if buffer.Cap() < PacketHeaderLen {
+		return errors.NewError("write bytes len too short, minnum is "+strconv.Itoa(PacketHeaderLen)+"bytes", nil)
+	}
+	buf.WriteBytes(buffer, common.Uint16ToBytes(h.Len))
+	buf.WriteBytes(buffer, common.Uint32ToBytes(h.Rid))
+	buf.WriteByte(buffer, h.Protocol)
+	buf.WriteBytes(buffer, h.Reserved[:])
+	buf.WriteByte(buffer, h.Status)
+	return nil
+}
 
 func (h *PacketHeader) ReadFrom(buffer buf.Buffer) error {
 	if buffer.Len() < PacketHeaderLen {
@@ -44,11 +40,6 @@ func (h *PacketHeader) ReadFrom(buffer buf.Buffer) error {
 	h.Len, _ = buf.ReadUint16(buffer)
 	h.Rid, _ = buf.ReadUint32(buffer)
 	h.Protocol, _ = buf.ReadByte(buffer)
-	{
-		hostPort, _ := buf.ReadBytes(buffer, 6)
-		h.Host = net.ParseFromBytes(hostPort[:5])
-		h.Port = net.Port(common.Uint16(hostPort[5:]))
-	}
 	{
 		for range h.Reserved {
 			_, _ = buf.ReadByte(buffer)
